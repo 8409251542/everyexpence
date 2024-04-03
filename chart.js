bd2ae1b6a9ae083c1cfd6561951b7136fd378a2d
Ravi Shankar <ravi.yoof@gmail.com>
2024-04-03 14:16:59 +0530

    day_3

---
+++ chart.js
@@ -0,0 +1,22 @@
+
+const ctx = document.getElementById('myChart');
+
+ var chart= new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: ['Earnning','Spending','Remaining'],
+      datasets: [{
+        label: '# of Votes',
+        data: [eraning,spendinng,remaining],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+      }
+    }
+});
+function updateChart(){
+    chart.data.datasets[0].data=[eraning,spendinng,remaining];
+    chart.update();
+}
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+let transactionData=[];
 let addBtn = document.getElementById("addSpend");
 let pop = document.querySelector(".popup");
+
 addBtn.addEventListener('click',popUp);
 function popUp(){
     pop.style.display='flex';
@@ -9,14 +11,62 @@ closeBtn.addEventListener('click',closePop);
 function closePop(){
     pop.style.display='none';
 }
+let ul=document.getElementById("list")
 let addTrn = document.querySelector(".submit");
-addTrn.addEventListener('click',addTrasaction);
-function addTrasaction(){
-    let desc=document.getElementById("Desc");
-    let amt=document.getElementById("amt");
-    let date=document.getElementById("date");
+let transactionForm=document.getElementById("transactionForm");
+transactionForm.addEventListener('submit',addTrasaction);
+let eraning=0,spendinng=0,remaining=0;
+
+function addTrasaction(e){
+    e.preventDefault();
+    let desc=document.getElementById("Desc").value;
+    let amt=document.getElementById("amt").value;
+    let date=document.getElementById("date").value;
     let check=document.getElementById("type");
-    if(check==checked){
-        console.log("income");
+    let earn=document.getElementById("income");
+    let spend=document.getElementById("expence")
+    let remain=document.getElementById("remain");
+    
+    if(check.checked){
+       // console.log("income");
+        transactionData.push({
+            "Date":date,
+            "Amount":amt,
+            "Description":desc,
+            "Type":"Income"
+        })
+        eraning+=Number(amt);
+        remaining+=Number(amt);
+        let list = document.createElement('li');
+        list.className="plus";
+        list.innerHTML= `${desc} <span> ₹${amt}</span>`;
+        ul.appendChild(list);
+        earn.innerText=`₹${eraning}`;
+        remain.innerText=`₹${remaining}`;
+        
     }
+    else{
+        //console.log("expense");
+        transactionData.push({
+            "Date":date,
+            "Amount":-amt,
+            "Description":desc,
+            "Type":"expense"
+        })
+        spendinng+=Number(amt);
+        remaining-=Number(amt);
+        let list = document.createElement('li');
+        list.className="minus";
+        list.innerHTML= `${desc} <span> -₹${amt}</span>`;
+        ul.appendChild(list);
+        remain.innerText=`₹${remaining}`;
+        spend.innerText=`₹${spendinng}`;
+        
+    }
+   // console.log(desc,amt,date);
+   updateChart();
 }
+console.log(transactionData);
+// chart
+
+//
